e5247fae7bf58a4e37bbe06d18706d8da06be479
chutebnl <braianledantes@gmail.com>
2019-09-20 16:53:55 -0300

    no me agrego la clase Mesa

---
+++ src/Filosofos/Mesa.java
@@ -0,0 +1,47 @@
+package Filosofos;
+
+import java.util.Random;
+import java.util.concurrent.Semaphore;
+
+public class Mesa {
+
+    private int cant;
+    private Semaphore[] tenedores;
+    private Random random;
+
+    public Mesa(int cant) {
+        this.cant = cant;
+        random = new Random(System.currentTimeMillis());
+        tenedores = new Semaphore[cant];
+
+        for (int i = 0; i < cant; i++) {
+            tenedores[i] = new Semaphore(1, false);
+        }
+    }
+    public void comer(Filosofo filosofo) {
+        Semaphore tenedor1 = tenedores[filosofo.getPos()];
+        Semaphore tenedor2 = tenedores[(filosofo.getPos() + 1) % cant];
+        boolean tomo1 = tenedor1.tryAcquire();
+        boolean tomo2 = tenedor2.tryAcquire();
+
+        if (tomo1 && tomo2) {
+            try {
+                System.out.println("\u001B[31m" + filosofo.getName() + " estoy COMIENDO <----");
+                Thread.sleep(1000 * (1 + random.nextInt(3)));
+                System.out.println("\u001B[0m"+filosofo.getName() + " DEJÉ DE COMER  ---->");
+                tenedor1.release();
+                tenedor2.release();
+                //Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        } else {
+            if (tomo1) {
+                tenedor1.release();
+            }
+            if (tomo2) {
+                tenedor2.release();
+            }
+        }
+    }
+}
